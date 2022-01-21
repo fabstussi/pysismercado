7aa8aa00735e82ff6677d8891249807b6dd02b07
Fabiano Stussi Pereria <66576716+fabstussi@users.noreply.github.com>
2022-01-21 15:31:44 -0300

    Finalizada a classe model

--- model.py
+++ model.py
@@ -4,8 +4,47 @@ class Categorias:
         self.nome = nome
         self.descricao = descricao
 
-    def __str__(self):
-        return f'{self.nome}'
+
+class Produtos:
+    def __init__(self, id, categoria, nome, quantidade, custo, preco, descricao):
+        self.id = id
+        self.categoria = categoria
+        self.nome = nome
+        self.quantidade = quantidade
+        self.custo = custo
+        self.preco = preco
+        self.descricao = descricao
 
 
+class Clientes:
+    def __init__(self, cpf, nome, telefone, sexo, ano_nasc):
+        self.cpf = cpf
+        self.nome = nome
+        self.telefone = telefone
+        self.sexo = sexo
+        self.ano_nasc = ano_nasc
 
+
+class Cargos:
+    def __init__(self, id, nome, descricao):
+        self.id = id
+        self.nome = nome
+        self.descricao = descricao
+
+
+class Vendedores(Clientes):
+    def __init__(self, cpf, nome, telefone, sexo, ano_nasc, cargo):
+        super().__init__(cpf, nome, telefone, sexo, ano_nasc)
+        self.cargo = cargo
+
+
+class Vendas:
+    def __init__(self, cupom, vendedor, data, cliente, quantidade, produto, preco, valor):
+        self.cupom = cupom
+        self.vendedor = vendedor
+        self.data = data
+        self.cliente = cliente
+        self.quantidade = quantidade
+        self.produto = produto
+        self.preco = preco
+        self.valor = valor

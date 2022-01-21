66b9ecac82850d8f0b056f0c0aa8c2e4a46b1302
Fabiano Stussi Pereria <66576716+fabstussi@users.noreply.github.com>
2022-01-21 18:46:02 -0300

    Adicionados alguns id em algumas classes e arrumado o código para ficar dentro das normas da pep8

--- model.py
+++ model.py
@@ -6,7 +6,8 @@ class Categorias:
 
 
 class Fornecerdores:
-    def __init__(self, cnpj, nome, telefone, categoria):
+    def __init__(self, id, cnpj, nome, telefone, categoria):
+        self.id = id
         self.cnpj = cnpj
         self.nome = nome
         self.telefone = telefone
@@ -14,9 +15,11 @@ class Fornecerdores:
 
 
 class Produtos:
-    def __init__(self, id, categoria, nome, quantidade, custo, preco, descricao):
+    def __init__(self, id, categoria, fornecedor, nome, quantidade, custo,
+                 preco, descricao):
         self.id = id
         self.categoria = categoria
+        self.fornecedor = fornecedor
         self.nome = nome
         self.quantidade = quantidade
         self.custo = custo
@@ -25,7 +28,8 @@ class Produtos:
 
 
 class Clientes:
-    def __init__(self, cpf, nome, telefone, sexo, ano_nasc):
+    def __init__(self, id, cpf, nome, telefone, sexo, ano_nasc):
+        self.id = id
         self.cpf = cpf
         self.nome = nome
         self.telefone = telefone
@@ -41,13 +45,14 @@ class Cargos:
 
 
 class Vendedores(Clientes):
-    def __init__(self, cpf, nome, telefone, sexo, ano_nasc, cargo):
-        super().__init__(cpf, nome, telefone, sexo, ano_nasc)
+    def __init__(self, id, cpf, nome, telefone, sexo, ano_nasc, cargo):
+        super().__init__(id, cpf, nome, telefone, sexo, ano_nasc)
         self.cargo = cargo
 
 
 class Vendas:
-    def __init__(self, cupom, vendedor, data, cliente, quantidade, produto, preco, valor):
+    def __init__(self, cupom, vendedor, data, cliente, quantidade, produto,
+                 preco, valor):
         self.cupom = cupom
         self.vendedor = vendedor
         self.data = data

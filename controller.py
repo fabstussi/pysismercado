9e2170a58c45753a25e8a3eb2d239185fe6d172f
Fabiano Stussi Pereria <66576716+fabstussi@users.noreply.github.com>
2022-01-25 14:31:54 -0300

    Criada a controller produto

--- controller.py
+++ controller.py
@@ -1,7 +1,7 @@
 from locale import setlocale, LC_ALL
 from datetime import datetime, timedelta
 from dao import CategoriasDao, FornecedoresDao, ProdutosDao, ClientesDao, CargosDao, VendedoresDao, VendasDao
-from model import Categorias, Fornecerdores
+from model import Categorias, Fornecerdores, Produtos, Clientes, Cargos, Vendedores, Vendas
 
 
 setlocale(LC_ALL, 'pt_BR.UTF-8')
@@ -150,11 +150,76 @@ class FornecedoresController:
             return False
 
 
+class ProdutosController:
+
+    @classmethod
+    def buscar(cls, id=None, nome=None) -> list:
+        produtos = ProdutosDao()
+        if id is not None:
+            listas = list(filter(lambda x: int(x.id) == id,
+                                 produtos.listar()))
+            if len(listas) > 0:
+                return listas
+            else:
+                return []
+        elif nome is not None:
+            listas = list(filter(lambda x: x.nome ==
+                          nome, produtos.listar()))
+            if len(listas) > 0:
+                return listas
+            else:
+                return []
+        else:
+            return produtos.listar()
+
+    @classmethod
+    def cadastrar(cls, nome: str, descricao: str,
+                  categoria: str, fornecedor: str, quantidade: int,
+                  preco: float) -> bool:
+        produtos = ProdutosDao()
+        listas = cls.buscar(nome=nome)
+        if len(listas) > 0:
+            return False
+        else:
+            id = produtos.gera_id()
+            if id == -1:
+                return False
+            resposta = produtos.salvar(Produtos(id, nome, descricao,
+                                                categoria, fornecedor,
+                                                quantidade, preco), 'a')
+            return resposta
+
+    @classmethod
+    def editar(cls, id: int, novo_nome: str, descricao: str,
+               nova_categoria: str, novo_fornecedor: str, nova_quantidade: int,
+               novo_preco: float) -> bool:
+        produtos = ProdutosDao()
+        listas = cls.buscar(id=id)
+        if len(listas) > 0:
+            listas = cls.buscar()
+            for i, produto in enumerate(listas):
+                modo = 'w' if i == 0 else 'a'
+                if produto.id != id:
+                    produtos.salvar(produto, modo)
+                else:
+                    produtos.salvar(Produtos(id, novo_nome, descricao,
+                                             nova_categoria, novo_fornecedor,
+                                             nova_quantidade, novo_preco), modo)
+            return True
+        else:
+            return False
+
+    @classmethod
+    def excluir(cls, id: int) -> bool:
+        produtos = ProdutosDao()
+        listas = cls.buscar(id=id)
+        if len(listas) > 0:
+            listas = cls.buscar()
+            for i, produto in enumerate(listas):
+                modo = 'w' if i == 0 else 'a'
+                if produto.id != id:
+                    produtos.salvar(produto, modo)
+
+
 if __name__ == '__main__':
-    resposta = FornecedoresController.cadastrar(
-        '12345678901234', 'Fornecedor 2', '12345678901', 'Categoria 2')
-    print('ok' if resposta else 'erro')
-    input()
-    print(FornecedoresController.buscar(nome='Fornecedor 1'))
-    print(FornecedoresController.buscar(id=1))
-    print(FornecedoresController.buscar())
+    pass

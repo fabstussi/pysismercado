329814d54b60ce9def481ab0634a0c99548b5fac
Fabiano Stussi Pereria <66576716+fabstussi@users.noreply.github.com>
2022-01-26 17:39:55 -0300

    Modificações necessárias, nas classes, foi adicionada a tranformação da str visivel para o bool visivel na hora de instanciar as classes

--- dao.py
+++ dao.py
@@ -6,11 +6,11 @@ class CategoriasDao:
     @classmethod
     def listar(cls) -> list:
         try:
-            with open('categorias.txt', 'r') as arquivo:
+            with open('pydb/categorias.txt', 'r') as arquivo:
                 linhas = arquivo.readlines()
             linhas = list(map(lambda linha: linha.replace('\n', ''), linhas))
             linhas = list(map(lambda linha: linha.split('|'), linhas))
-            return [Categorias(linha[0], linha[1], linha[2])
+            return [Categorias(linha[0], linha[1], linha[2], bool(linha[3]))
                     for linha in linhas]
         except FileNotFoundError:
             return []
@@ -21,9 +21,10 @@ class CategoriasDao:
     @classmethod
     def salvar(cls, categoria: Categorias, modo: str) -> bool:
         try:
-            with open('categorias.txt', modo) as arquivo:
+            with open('pydb/categorias.txt', modo) as arquivo:
                 arquivo.write(
-                    f'{categoria.id}|{categoria.nome}|{categoria.descricao}\n')
+                    f'{categoria.id}|{categoria.nome}|{categoria.descricao}|' +
+                    f'{categoria.visivel}\n')
             return True
         except Exception:
             return False
@@ -31,7 +32,7 @@ class CategoriasDao:
     @staticmethod
     def gera_id() -> int:
         try:
-            with open('categorias.txt', 'r') as arquivo:
+            with open('pydb/categorias.txt', 'r') as arquivo:
                 linhas = arquivo.readlines()
             ultma_linha = [linhas.strip() for linhas in linhas][-1]
             if len(ultma_linha) == 0:
@@ -49,12 +50,13 @@ class FornecedoresDao:
     @classmethod
     def listar(cls) -> list:
         try:
-            with open('fornecedores.txt', 'r') as arquivo:
+            with open('pydb/fornecedores.txt', 'r') as arquivo:
                 linhas = arquivo.readlines()
             linhas = list(map(lambda linha: linha.replace('\n', ''), linhas))
             linhas = list(map(lambda linha: linha.split('|'), linhas))
             return [Fornecerdores(linha[0], linha[1], linha[2], linha[3],
-                                  linha[4]) for linha in linhas]
+                                  linha[4], bool(linha[5])
+                                  ) for linha in linhas]
         except FileNotFoundError:
             return []
         except Exception as e:
@@ -64,10 +66,11 @@ class FornecedoresDao:
     @classmethod
     def salvar(cls, fornecedor: Fornecerdores, modo: str) -> bool:
         try:
-            with open('fornecedores.txt', modo) as arquivo:
+            with open('pydb/fornecedores.txt', modo) as arquivo:
                 arquivo.write(
                     f'{fornecedor.id}|{fornecedor.cnpj}|{fornecedor.nome}|' +
-                    f'{fornecedor.telefone}|{fornecedor.categoria}\n')
+                    f'{fornecedor.telefone}|{fornecedor.categoria}|' +
+                    f'{fornecedor.visivel}\n')
             return True
         except Exception:
             return False
@@ -75,7 +78,7 @@ class FornecedoresDao:
     @staticmethod
     def gera_id() -> int:
         try:
-            with open('fornecedores.txt', 'r') as arquivo:
+            with open('pydb/fornecedores.txt', 'r') as arquivo:
                 linhas = arquivo.readlines()
             ultma_linha = [linhas.strip() for linhas in linhas][-1]
             if len(ultma_linha) == 0:
@@ -93,13 +96,13 @@ class ProdutosDao:
     @classmethod
     def listar(cls) -> list:
         try:
-            with open('produtos.txt', 'r') as arquivo:
+            with open('pydb/produtos.txt', 'r') as arquivo:
                 linhas = arquivo.readlines()
             linhas = list(map(lambda linha: linha.replace('\n', ''), linhas))
             linhas = list(map(lambda linha: linha.split('|'), linhas))
             return [Produtos(linha[0], linha[1], linha[2], linha[3], linha[4],
-                             linha[5], linha[6],
-                             linha[7],) for linha in linhas]
+                             linha[5], linha[6], linha[7], bool(linha[8])
+                             ) for linha in linhas]
         except FileNotFoundError:
             return []
         except Exception as e:
@@ -109,11 +112,11 @@ class ProdutosDao:
     @classmethod
     def salvar(cls, produto: Produtos, modo: str) -> bool:
         try:
-            with open('produtos.txt', modo) as arquivo:
+            with open('pydb/produtos.txt', modo) as arquivo:
                 arquivo.write(
                     f'{produto.id}|{produto.categoria}|{produto.fornecedor}' +
                     f'|{produto.nome}|{produto.quantidade}|{produto.custo}|' +
-                    f'{produto.preco}|{produto.descricao}\n')
+                    f'{produto.preco}|{produto.descricao}|{produto.visivel}\n')
             return True
         except Exception:
             return False
@@ -121,7 +124,7 @@ class ProdutosDao:
     @staticmethod
     def gera_id() -> int:
         try:
-            with open('produtos.txt', 'r') as arquivo:
+            with open('pydb/produtos.txt', 'r') as arquivo:
                 linhas = arquivo.readlines()
             ultma_linha = [linhas.strip() for linhas in linhas][-1]
             if len(ultma_linha) == 0:
@@ -139,12 +142,12 @@ class ClientesDao:
     @classmethod
     def listar(cls) -> list:
         try:
-            with open('clientes.txt', 'r') as arquivo:
+            with open('pydb/clientes.txt', 'r') as arquivo:
                 linhas = arquivo.readlines()
             linhas = list(map(lambda linha: linha.replace('\n', ''), linhas))
             linhas = list(map(lambda linha: linha.split('|'), linhas))
             return [Clientes(linha[0], linha[1], linha[2], linha[3], linha[4],
-                             linha[5]) for linha in linhas]
+                             linha[5], bool(linha[6])) for linha in linhas]
         except FileNotFoundError:
             return []
         except Exception as e:
@@ -154,10 +157,11 @@ class ClientesDao:
     @classmethod
     def salvar(cls, cliente: Clientes, modo: str) -> bool:
         try:
-            with open('clientes.txt', modo) as arquivo:
+            with open('pydb/clientes.txt', modo) as arquivo:
                 arquivo.write(
                     f'{cliente.id}|{cliente.cpf}|{cliente.nome}|' +
-                    f'{cliente.telefone}|{cliente.sexo}|{cliente.ano_nasc}\n')
+                    f'{cliente.telefone}|{cliente.sexo}|{cliente.ano_nasc}|' +
+                    f'{cliente.visivel}\n')
             return True
         except Exception:
             return False
@@ -165,7 +169,7 @@ class ClientesDao:
     @staticmethod
     def gera_id() -> int:
         try:
-            with open('clientes.txt', 'r') as arquivo:
+            with open('pydb/clientes.txt', 'r') as arquivo:
                 linhas = arquivo.readlines()
             ultma_linha = [linhas.strip() for linhas in linhas][-1]
             if len(ultma_linha) == 0:
@@ -183,11 +187,12 @@ class CargosDao:
     @classmethod
     def listar(cls) -> list:
         try:
-            with open('cargos.txt', 'r') as arquivo:
+            with open('pydb/cargos.txt', 'r') as arquivo:
                 linhas = arquivo.readlines()
             linhas = list(map(lambda linha: linha.replace('\n', ''), linhas))
             linhas = list(map(lambda linha: linha.split('|'), linhas))
-            return [Cargos(linha[0], linha[1], linha[2]) for linha in linhas]
+            return [Cargos(linha[0], linha[1], linha[2], bool(linha[4])
+                           ) for linha in linhas]
         except FileNotFoundError:
             return []
         except Exception as e:
@@ -197,8 +202,9 @@ class CargosDao:
     @classmethod
     def salvar(cls, cargo: Cargos, modo: str) -> bool:
         try:
-            with open('cargos.txt', modo) as arquivo:
-                arquivo.write(f'{cargo.id}|{cargo.nome}|{cargo.descricao}\n')
+            with open('pydb/cargos.txt', modo) as arquivo:
+                arquivo.write(f'{cargo.id}|{cargo.nome}|{cargo.descricao}|' +
+                              f'{cargo.visivel}\n')
             return True
         except Exception:
             return False
@@ -206,7 +212,7 @@ class CargosDao:
     @staticmethod
     def gera_id() -> int:
         try:
-            with open('cargos.txt', 'r') as arquivo:
+            with open('pydb/cargos.txt', 'r') as arquivo:
                 linhas = arquivo.readlines()
             ultma_linha = [linhas.strip() for linhas in linhas][-1]
             if len(ultma_linha) == 0:
@@ -224,13 +230,13 @@ class FuncionariosDao:
     @classmethod
     def listar(cls) -> list:
         try:
-            with open('funcionarios.txt', 'r') as arquivo:
+            with open('pydb/funcionarios.txt', 'r') as arquivo:
                 linhas = arquivo.readlines()
             linhas = list(map(lambda linha: linha.replace('\n', ''), linhas))
             linhas = list(map(lambda linha: linha.split('|'), linhas))
             return [Funcionarios(linha[0], linha[1], linha[2], linha[3],
-                               linha[4], linha[5],
-                               linha[6]) for linha in linhas]
+                                 linha[4], linha[5], linha[6], bool(linha[7])
+                                 ) for linha in linhas]
         except FileNotFoundError:
             return []
         except Exception as e:
@@ -238,13 +244,14 @@ class FuncionariosDao:
             return []
 
     @classmethod
-    def salvar(cls, vendedor: Funcionarios, modo: str) -> bool:
+    def salvar(cls, funcionario: Funcionarios, modo: str) -> bool:
         try:
-            with open('funcionarios.txt', modo) as arquivo:
+            with open('pydb/funcionarios.txt', modo) as arquivo:
                 arquivo.write(
-                    f'{vendedor.id}|{vendedor.cpf}|{vendedor.nome}|' +
-                    f'{vendedor.telefone}|{vendedor.sexo}|' +
-                    f'{vendedor.ano_nasc}|{vendedor.cargo}\n')
+                    f'{funcionario.id}|{funcionario.cpf}|{funcionario.nome}|' +
+                    f'{funcionario.telefone}|{funcionario.sexo}|' +
+                    f'{funcionario.ano_nasc}|{funcionario.cargo}|' +
+                    f'{funcionario.visivel}\n')
             return True
         except Exception:
             return False
@@ -252,7 +259,7 @@ class FuncionariosDao:
     @staticmethod
     def gera_id() -> int:
         try:
-            with open('funcionarios.txt', 'r') as arquivo:
+            with open('pydb/funcionarios.txt', 'r') as arquivo:
                 linhas = arquivo.readlines()
             ultma_linha = [linhas.strip() for linhas in linhas][-1]
             if len(ultma_linha) == 0:
@@ -270,12 +277,12 @@ class VendasDao:
     @classmethod
     def listar(cls) -> list:
         try:
-            with open('vendas.txt', 'r') as arquivo:
+            with open('pydb/vendas.txt', 'r') as arquivo:
                 linhas = arquivo.readlines()
             linhas = list(map(lambda linha: linha.replace('\n', ''), linhas))
             linhas = list(map(lambda linha: linha.split('|'), linhas))
             return [Vendas(linha[0], linha[1], linha[2], linha[3], linha[4],
-                           linha[5], linha[6], linha[7]) for linha in linhas]
+                           linha[5], bool(linha[6])) for linha in linhas]
         except FileNotFoundError:
             return []
         except Exception as e:
@@ -285,11 +292,11 @@ class VendasDao:
     @classmethod
     def salvar(cls, venda: Vendas, modo: str) -> bool:
         try:
-            with open('vendas.txt', modo) as arquivo:
+            with open('pydb/vendas.txt', modo) as arquivo:
                 arquivo.write(
                     f'{venda.cupom}|{venda.funcionario}|{venda.data}|' +
-                    f'{venda.cliente}|{venda.quantidade}|{venda.produto}|' +
-                    f'{venda.preco}|{venda.valor}\n')
+                    f'{venda.cliente}|{venda.compra}|{venda.valor}|' +
+                    f'{venda.visivel}\n')
             return True
         except Exception:
             return False
@@ -297,7 +304,7 @@ class VendasDao:
     @staticmethod
     def gera_cupom() -> int:
         try:
-            with open('vendas.txt', 'r') as arquivo:
+            with open('pydb/vendas.txt', 'r') as arquivo:
                 linhas = arquivo.readlines()
             ultma_linha = [linhas.strip() for linhas in linhas][-1]
             if len(ultma_linha) == 0:
@@ -311,5 +318,4 @@ class VendasDao:
 
 
 if __name__ == '__main__':
-    # from time import sleep
     pass

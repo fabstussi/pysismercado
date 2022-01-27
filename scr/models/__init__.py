b0b4295da2deb1cdd11ce01240775776e00ab131
Fabiano Stussi Pereria <66576716+fabstussi@users.noreply.github.com>
2022-01-27 18:26:06 -0300

    Retirado o código de importação das classes do módulo, se mostraram inuteis, deixado o código sys e os que se mostraram essenciais

--- scr/models/__init__.py
+++ scr/models/__init__.py
@@ -1,10 +1,3 @@
-from configparser import InterpolationMissingOptionError
-from categorias import Categorias
-from fornecedores import Fornecerdores
-from produtos import Produtos
-from pessoas import Clientes, Funcionarios
-from cargos import Cargos
-from vendas import Vendas
 import sys
 import os
 

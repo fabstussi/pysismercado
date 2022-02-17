e1f729a0615a3e9d7d7ac7298044a30fa79d76e4
Fabiano Stussi Pereria <66576716+fabstussi@users.noreply.github.com>
2022-02-17 10:37:46 -0300

    Arrumado o código
    Cupom será uma string e não um inteiro.
    Colocado float para o retorno das variáveis custo e preço.
    Adicionados os métodos cria_cupom e cria_relatório (por enquanto sem código.
    O código do método gera_cupom foi substituído ao invés de gerar automático a partir do ultimo cadastrado, ele vai gerar pela data e hora.

--- scr/dals/vendas.py
+++ scr/dals/vendas.py
@@ -1,4 +1,5 @@
 from models.vendas import Vendas
+from util.PyNumBR import pega_data, pega_hora
 
 
 class VendasDal:
@@ -10,8 +11,8 @@ class VendasDal:
                 linhas = arquivo.readlines()
             linhas = list(map(lambda linha: linha.replace('\n', ''), linhas))
             linhas = list(map(lambda linha: linha.split('|'), linhas))
-            return [Vendas(int(linha[0]), linha[1], linha[2], linha[3],
-                           linha[4], linha[5], int(linha[6])
+            return [Vendas(linha[0], linha[1], linha[2], linha[3],
+                           float(linha[4]), float(linha[5]), int(linha[6])
                            ) for linha in linhas]
         except FileNotFoundError:
             return []
@@ -31,17 +32,26 @@ class VendasDal:
         except Exception:
             return False
 
+    @classmethod
+    def cria_cupom(cls, venda: Vendas) -> bool:
+        pass
+        # try:
+        #     with open(f'cupom_{venda.cupom}.txt', 'w') as arquivo:
+        #         arquivo.write(
+        #             f'{venda.cupom}\n' +
+        #             f'{venda.funcionario}\n' +
+        #             f'{venda.data}\n' +
+        #             f'{venda.cliente}\n' +
+        #             f'{venda.compra}\n' +
+        #             f'{venda.valor}\n' +
+        #             f'{venda.visivel}\n')
+
+    @classmethod
+    def cria_relatorio(cls) -> bool:
+        pass
+
     @staticmethod
-    def gera_cupom() -> int:
-        try:
-            with open('db/vendas.dbpy', 'r') as arquivo:
-                linhas = arquivo.readlines()
-            ultma_linha = [linhas.strip() for linhas in linhas][-1]
-            if len(ultma_linha) == 0:
-                return 1
-            return int(ultma_linha.split('|')[0]) + 1
-        except FileNotFoundError:
-            return 1
-        except Exception as e:
-            print(f'Erro: {e}')
-            return -1
+    def gera_cupom() -> str:
+        data = pega_data().strip('/')
+        hora = pega_hora().strip(':')
+        return f'{data[2]}{data[1]}{data[0]}{hora[2]}{hora[1]}'
